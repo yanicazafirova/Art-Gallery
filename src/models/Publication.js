5d6711545ccf67250caebc1ee4f057a7aed686d8
Yanica Zafirova <yanica_zafirova@abv.bg>
2023-02-10 12:55:15 +0200

    Create publication model

---
+++ src/models/Publication.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const publicationSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: [true, 'Title is required!'],
+        minLength: [6, 'Title should be a minimum 6 characters long!'],
+    },
+    paintingTechnique: {
+        type: String,
+        required: [true, 'Painting technique is required!'],
+        maxLength: [15, 'Painting technique should be a maximum 15 characters long!'],
+    },
+    artPicture: {
+        type: String,
+        required: [true, 'Art picture is required!'],
+        validate: [/^https?:\/\//i, 'Image URL should start with http/https!'],
+    },
+    certificate: {
+        enum: ['Yes', 'No'],
+        required: [true, 'Certificate is required!'],
+    },
+    author: {
+        type: mongoose.Types.ObjectId,
+        ref: 'User',
+    },
+    usersShared: [{
+        type: mongoose.Types.ObjectId,
+        ref: 'User',
+    }],
+});
+
+const Publicaton = mongoose.model('Publicaton', publicationSchema);
+
+module.exports = Publicaton;

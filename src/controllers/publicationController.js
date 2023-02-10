0ceea4688a908c600b8e22cbfb88ae48ffdbd6ab
Yanica Zafirova <yanica_zafirova@abv.bg>
2023-02-10 15:05:56 +0200

    Add gallery page

--- src/controllers/publicationController.js
+++ src/controllers/publicationController.js
@@ -14,11 +14,21 @@ router.post('/create', isAuth, async (req, res) => {
 
     try {
         await publicationService.create({ title, paintingTechnique, artPicture, certificate, author: req.user._id });
-        
+
         res.redirect('/publications/gallery');
     } catch (error) {
         res.render('publication/create', { error: getErrorMessage(error) });
     }
 });
 
+router.get('/gallery', async (req, res) => {
+    const publications = await publicationService.getAllPublications().lean();
+    console.log(publications);
+    res.render('publication/gallery', { publications });
+});
+
+router.get('/:publicationId/details', async (req, res) => {
+    const publication = await publicationService.getOneById(req.params.publicationId).lean();
+    res.render('publication/details', { publication });
+});
 module.exports = router;
--- src/services/publicationService.js
+++ src/services/publicationService.js
@@ -1,4 +1,11 @@
 const Publication = require('../models/Publication');
 
 
-exports.create = (publicationData) => Publication.create(publicationData); 
+exports.create = (publicationData) => Publication.create(publicationData);
+
+exports.getOneById = (publicationId) => Publication.findById(publicationId);
+
+
+
+
+exports.getAllPublications = () => Publication.find();

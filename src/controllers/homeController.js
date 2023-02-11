49045d5128566ccb0d65608757e85da174f0514f
Yanica Zafirova <yanica_zafirova@abv.bg>
2023-02-11 17:47:49 +0200

    Update home controller

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 
-router.get('/', (req, res) => {
-    res.render('home');
+const publicationService = require('../services/publicationService');
+
+router.get('/', async (req, res) => {
+    const publicationsResult = await publicationService.getAllPublications().lean();
+    const publications = publicationsResult.map(x => ({ ...x, sharesCount: x.usersShared.length }));
+    res.render('home', { publications });
 });
 
 module.exports = router;

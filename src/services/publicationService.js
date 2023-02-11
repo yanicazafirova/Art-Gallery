47bce15f264f08857d56be194220e39fc21996b3
Yanica Zafirova <yanica_zafirova@abv.bg>
2023-02-11 17:47:14 +0200

    Fix edit function

--- src/services/publicationService.js
+++ src/services/publicationService.js
@@ -5,8 +5,9 @@ exports.create = (publicationData) => Publication.create(publicationData);
 
 exports.getOneById = (publicationId) => Publication.findById(publicationId);
 
-exports.update = (publicationId, newData) => Publication.findByIdAndUpdate(publicationId, newData, { runValidators: true });
+exports.getOneDetailed = (publicationId) => Publication.findById(publicationId).populate('author');
 
+exports.update = (publicationId, newData) => Publication.findByIdAndUpdate(publicationId, newData, { runValidators: true });
 
 exports.deleteById = (publicationId) => Publication.findByIdAndDelete(publicationId);
 

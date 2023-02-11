2cbbb0879e9a92503e8ce96f4fdfa84f158d0474
Yanica Zafirova <yanica_zafirova@abv.bg>
2023-02-11 17:46:21 +0200

    Add functionality to home page

--- src/controllers/publicationController.js
+++ src/controllers/publicationController.js
@@ -29,30 +29,40 @@ router.get('/gallery', async (req, res) => {
 });
 
 router.get('/:publicationId/details', async (req, res) => {
-    const publication = await publicationService.getOneById(req.params.publicationId).lean();
-    const isAuthor = publication.author == req.user?._id;
-    res.render('publication/details', { publication, isAuthor });;
+    const publication = await publicationService.getOneDetailed(req.params.publicationId).lean();
+    const isAuthor = publication.author._id == req.user?._id;
+    const isShared = publication.usersShared.some(x => x._id == req.user._id);
+
+    res.render('publication/details', { ...publication, isAuthor, isShared });;
 });
 
-router.get('/:publicationId/edit', checkIsOwner, async (req, res) => {
+router.get('/:publicationId/edit', isAuth, checkIsOwner, async (req, res) => {
     const publication = await publicationService.getOneById(req.params.publicationId).lean();
-    res.render('publication/edit', { publication });
+    res.render('publication/edit', { ...publication });
 });
 
-router.post('/:publicationId/edit', checkIsOwner, async (req, res) => {
+router.post('/:publicationId/edit', isAuth, checkIsOwner, async (req, res) => {
     const { title, paintingTechnique, artPicture, certificate } = req.body;
 
     try {
-        const publication = await publicationService.update(req.params.publicationId, { title, paintingTechnique, artPicture, certificate }).lean();
+        await publicationService.update(req.params.publicationId, { title, paintingTechnique, artPicture, certificate }).lean();
         res.redirect(`/publications/${req.params.publicationId}/details`);
     } catch (error) {
         res.render('publication/edit', { error: error.message });
     }
 });
 
-router.get('/:publicationId/delete', checkIsOwner, async (req, res) => {
+router.get('/:publicationId/delete', isAuth, checkIsOwner, async (req, res) => {
     await publicationService.deleteById(req.params.publicationId);
     res.redirect('/publications/gallery');
+});
+
+router.get('/:publicationId/share', isAuth, async (req, res) => {
+    const publication = await publicationService.getOneById(req.params.publicationId);
+    publication.usersShared.push(req.user._id);
+    await publication.save();
+
+    res.redirect('/');
 });
 
 module.exports = router;

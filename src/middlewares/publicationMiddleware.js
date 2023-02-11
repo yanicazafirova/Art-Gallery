900fc8b9b4d0eae63d977f771b2aec20ce26b62d
Yanica Zafirova <yanica_zafirova@abv.bg>
2023-02-11 17:40:32 +0200

    Update publication middleware

--- src/middlewares/publicationMiddleware.js
+++ src/middlewares/publicationMiddleware.js
@@ -3,12 +3,14 @@ const publicationService = require('../services/publicationService');
 exports.isOwner = async (req, res, next) => {
     const publicationId = req.params.publicationId;
 
-    const publication = await publicationService.getOneById(publicationId);
+    const publication = await publicationService.getOneById(publicationId).lean();
 
     if (publication.author == req.user._id) {
-       
+
         return res.redirect(`/publication/${req.params.publicationId}/details`);
     }
+
+    req.publication = publication;
     next();
 };
 
@@ -16,8 +18,10 @@ exports.checkIsOwner = async (req, res, next) => {
     let publication = await publicationService.getOneById(req.params.publicationId);
 
     if (publication.author == req.user._id) {
+        req.publication = publication;
         next();
     } else {
         res.redirect(`/publication/${req.params.publicationId}/details`);
     }
+
 }
